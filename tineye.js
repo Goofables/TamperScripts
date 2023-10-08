6db9fa5639908273b4b4d4470e7ca32ee7a2c9ed
Goofables <Goofables@gmail.com>
2023-10-08 13:40:46 -0400

    Better Tineye

---
+++ tineye.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         Better Tineye
+// @version      0.1
+// @description  Check if images are still active
+// @author       Goofables
+// @match        https://tineye.com/*
+// @icon         https://www.google.com/s2/favicons?domain=tineye.com
+// @grant        none
+// ==/UserScript==
+
+function test_images() {
+    for (const e of document.getElementsByClassName("image-link")) {
+        const img = document.createElement("img");
+        img.src = e.getElementsByTagName("a")[0].href;
+        img.width = 20;
+        img.height = 20;
+        e.append(img);
+    }
+}
+
+
+setTimeout(
+    () => {
+        const button = document.createElement('button');
+        button.innerText = 'Test images';
+        button.addEventListener('click', test_images);
+        document.getElementById("results-div").prepend(button);
+    }, 1000
+)
+
