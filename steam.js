fa2abfeb4f86b5a240bfe6a6a45a6d5268c4643e
Goofables <Goofables@gmail.com>
2023-11-29 01:27:00 -0500

    Added steam activation code auto submission

---
+++ steam.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         SteamActivateSubmit
+// @version      0.1
+// @description  Auto submit steam product activation codes
+// @author       Goofables
+// @match        https://store.steampowered.com/account/registerkey
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=steampowered.com
+// ==/UserScript==
+
+
+function check_code() {
+    if (document.getElementById("error_display").innerHTML !== "") return;
+    let code = document.getElementById("product_key").value;
+    if (code.length < 7) return;
+    if (document.getElementById("registerkey_form").style.display !== "") return;
+    document.getElementById("accept_ssa").checked = true;
+    document.getElementById("register_btn").click()
+}
+
+setInterval(check_code,100);
